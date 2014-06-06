f7c3024eb3796859c27e71862b07ed2ce743caaa
Jessica Oros <orosjess@gmail.com>
2014-06-06 00:35:47 +0000

    Initial Commit

---
+++ pmpro-toolkit.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Plugin Name: PMPro Developer's Toolkit
+ * Author: Stranger Studios
+ * Description: Various tools to test and debug Paid Memberships Pro enabled websites.
+ * Version: .1
+ */
